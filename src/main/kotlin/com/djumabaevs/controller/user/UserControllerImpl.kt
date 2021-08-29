a8a4f2a8276ff9dca2c5ec395330733a77513076
Djumabaevs <djumabaevb@gmail.com>
2021-08-30 02:37:35 +0600

    override three methods

--- src/main/kotlin/com/djumabaevs/controller/user/UserControllerImpl.kt
+++ src/main/kotlin/com/djumabaevs/controller/user/UserControllerImpl.kt
@@ -1,9 +1,24 @@
 package com.djumabaevs.controller.user
 
+import com.djumabaevs.data.models.User
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 class UserControllerImpl(
     db: CoroutineDatabase
 ): UserController {
+
+    private val users = db.getCollection<User>()
+
+    override suspend fun createUser(user: User) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUserById(id: String): User? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getUserByEmail(email: String): User? {
+        TODO("Not yet implemented")
+    }
 
 }
