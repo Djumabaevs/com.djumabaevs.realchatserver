a8177b7137403d4a4b14190d64f1ac395b7e1fc7
Djumabaevs <djumabaevb@gmail.com>
2021-08-30 02:41:34 +0600

    install Koin main module in application

--- src/main/kotlin/com/djumabaevs/Application.kt
+++ src/main/kotlin/com/djumabaevs/Application.kt
@@ -1,7 +1,10 @@
 package com.djumabaevs
 
+import com.djumabaevs.di.mainModule
 import io.ktor.application.*
 import com.djumabaevs.plugins.*
+import org.koin.ktor.ext.Koin
+import org.koin.ktor.ext.modules
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
@@ -14,6 +17,9 @@ fun Application.module() {
     configureMonitoring()
     configureSerialization()
 //    configureSockets()
+    install(Koin) {
+        modules(mainModule)
+    }
 
 }
 
