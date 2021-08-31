cd4d9437f3ba850633dbd5cdba1ab21d1e99f762
Djumabaevs <djumabaevb@gmail.com>
2021-08-31 10:38:59 +0600

    call.respond basic api response

--- src/main/kotlin/com/djumabaevs/routes/UserRoutes.kt
+++ src/main/kotlin/com/djumabaevs/routes/UserRoutes.kt
@@ -2,12 +2,15 @@ package com.djumabaevs.routes
 
 import com.djumabaevs.controller.user.UserController
 import com.djumabaevs.data.requests.CreateAccountRequest
+import com.djumabaevs.data.responses.BasicApiResponse
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.koin.ktor.ext.inject
+import com.djumabaevs.util.*
+import com.djumabaevs.util.ApiResponseMessages.USER_ALREADY_EXISTS
 
 fun Route.userRoutes() {
     val userController: UserController by inject()
@@ -17,7 +20,16 @@ fun Route.userRoutes() {
                 call.respond(HttpStatusCode.BadRequest)
                 return@post
             }
-            val userExists =
+            val userExists = userController.getUserByEmail(request.email) != null
+            if(userExists) {
+                call.respond(
+                    BasicApiResponse(
+                        successful = false,
+                        message = USER_ALREADY_EXISTS
+                    )
+                )
+                return@post
+            }
         }
     }
 }
