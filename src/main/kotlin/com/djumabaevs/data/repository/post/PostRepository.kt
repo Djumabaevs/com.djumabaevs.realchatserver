16526a87796794d5966db8e6453fec34dfddabed
Djumabaevs <djumabaevb@gmail.com>
2021-08-31 12:07:24 +0600

    post repository impl

---
+++ src/main/kotlin/com/djumabaevs/data/repository/post/PostRepository.kt
@@ -0,0 +1,17 @@
+package com.djumabaevs.data.repository.post
+
+import com.djumabaevs.data.models.Post
+import com.djumabaevs.util.Constants
+
+interface PostRepository {
+
+    suspend fun createPostIfUserExists(post: Post): Boolean
+
+    suspend fun deletePost(postId: String)
+
+    suspend fun getPostsByFollows(
+        userId: String,
+        page: Int = 0,
+        pageSize: Int = Constants.DEFAULT_POST_PAGE_SIZE
+    ): List<Post>
+}
--- src/main/kotlin/com/djumabaevs/util/Constants.kt
+++ src/main/kotlin/com/djumabaevs/util/Constants.kt
@@ -3,4 +3,7 @@ package com.djumabaevs.util
 object Constants {
 
     val DATABASE_NAME = "real_chat_server"
+
+
+    const val DEFAULT_POST_PAGE_SIZE = 15
 }
