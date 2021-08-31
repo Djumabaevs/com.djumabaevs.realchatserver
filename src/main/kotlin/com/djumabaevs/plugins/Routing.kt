34392c54ea23593761b9fa33774cec942eb81efa
Djumabaevs <djumabaevb@gmail.com>
2021-08-31 10:42:59 +0600

    userRoutes in routing() plugin

--- src/main/kotlin/com/djumabaevs/plugins/Routing.kt
+++ src/main/kotlin/com/djumabaevs/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package com.djumabaevs.plugins
 
+import com.djumabaevs.routes.userRoutes
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.content.*
@@ -12,12 +13,6 @@ fun Application.configureRouting() {
     
 
     routing {
-        get("/") {
-                call.respondText("Hello World!")
-            }
-        // Static plugin. Try to access `/static/index.html`
-        static("/static") {
-            resources("static")
-        }
+   userRoutes()
     }
 }
