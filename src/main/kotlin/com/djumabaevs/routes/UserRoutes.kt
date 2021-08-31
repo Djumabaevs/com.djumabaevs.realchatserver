f700c7afab0ddc9c3d40c3440dfc3d0c50dd48c4
Djumabaevs <djumabaevb@gmail.com>
2021-08-31 10:33:18 +0600

    return@post

--- src/main/kotlin/com/djumabaevs/routes/UserRoutes.kt
+++ src/main/kotlin/com/djumabaevs/routes/UserRoutes.kt
@@ -1,9 +1,23 @@
 package com.djumabaevs.routes
 
 import com.djumabaevs.controller.user.UserController
+import com.djumabaevs.data.requests.CreateAccountRequest
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import org.koin.ktor.ext.inject
 
 fun Route.userRoutes() {
     val userController: UserController by inject()
+    route("/api/user/create") {
+        post {
+            val request = call.receiveOrNull<CreateAccountRequest>() ?: kotlin.run {
+                call.respond(HttpStatusCode.BadRequest)
+                return@post
+            }
+            val userExists =
+        }
+    }
 }
