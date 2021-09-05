823a02d9caf321c59513d40b1de97a1cc3eeab99
Djumabaevs <djumabaevb@gmail.com>
2021-09-05 10:46:57 +0600

    call util with JWT principal

--- src/main/kotlin/com/djumabaevs/plugins/Serialization.kt
+++ src/main/kotlin/com/djumabaevs/plugins/Serialization.kt
@@ -10,12 +10,6 @@ import io.ktor.routing.*
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         gson {
-            }
-    }
-
-    routing {
-        get("/json/gson") {
-                call.respond(mapOf("hello" to "world"))
-            }
+        }
     }
 }
