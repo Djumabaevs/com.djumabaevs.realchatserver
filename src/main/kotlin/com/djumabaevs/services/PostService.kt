fa3240a56ec16f4499ac53717622cf3e8a8f47bf
Djumabaevs <djumabaevb@gmail.com>
2021-09-06 10:39:30 +0600

    suspend functions in services

--- src/main/kotlin/com/djumabaevs/services/PostService.kt
+++ src/main/kotlin/com/djumabaevs/services/PostService.kt
@@ -3,6 +3,7 @@ package com.djumabaevs.services
 import com.djumabaevs.data.models.Post
 import com.djumabaevs.data.repository.post.PostRepository
 import com.djumabaevs.data.requests.CreatePostRequest
+import com.djumabaevs.util.Constants
 
 class PostService(
     private val repository: PostRepository
@@ -17,5 +18,17 @@ class PostService(
             )
         )
     }
-    suspend fun getPostForFollows
+    suspend fun getPostsForFollows(
+        userId: String,
+        page: Int = 0,
+        pageSize: Int = Constants.DEFAULT_POST_PAGE_SIZE
+    ): List<Post> {
+        return repository.getPostsByFollows(userId, page, pageSize)
+    }
+
+    suspend fun getPost(postId: String): Post? = repository.getPost(postId)
+
+    suspend fun deletePost(postId: String) {
+        repository.deletePost(postId)
+    }
 }
