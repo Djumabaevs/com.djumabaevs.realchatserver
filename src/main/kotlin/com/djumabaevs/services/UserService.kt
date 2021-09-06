6902c360d4d514a1a1ae9967ddf385fb4852aa53
Djumabaevs <djumabaevb@gmail.com>
2021-09-06 10:59:43 +0600

    more suspend functions

--- src/main/kotlin/com/djumabaevs/services/UserService.kt
+++ src/main/kotlin/com/djumabaevs/services/UserService.kt
@@ -1,11 +1,22 @@
 package com.djumabaevs.services
 
 import com.djumabaevs.data.repository.user.UserRepository
+import com.djumabaevs.data.requests.LoginRequest
 
 class UserService(
     private val repository: UserRepository
 ) {
     suspend fun doesUserWithEmailExists(email: String): Boolean {
         return repository.getUserByEmail(email) != null
+    }
+    suspend fun doesEmailBelongToUserId(email: String, userId: String): Boolean {
+        return repository.doesEmailBelongToUserId(email, userId)
+    }
+
+    suspend fun doesPasswordMatchForUser(request: LoginRequest): Boolean {
+        return repository.doesPasswordForUserMatch(
+            email = request.email,
+            enteredPassword = request.password
+        )
     }
 }
