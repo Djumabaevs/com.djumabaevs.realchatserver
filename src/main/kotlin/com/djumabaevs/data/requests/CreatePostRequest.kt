94b9055146ca7d5871c88e3420b397bee3e41c77
Djumabaevs <djumabaevb@gmail.com>
2021-09-07 14:36:36 +0600

    responses and requests refactor

--- src/main/kotlin/com/djumabaevs/data/requests/CreatePostRequest.kt
+++ src/main/kotlin/com/djumabaevs/data/requests/CreatePostRequest.kt
@@ -1,6 +1,5 @@
 package com.djumabaevs.data.requests
 
 data class CreatePostRequest(
-    val userId: String,
     val description: String
 )
--- src/main/kotlin/com/djumabaevs/data/requests/FollowUpdateRequest.kt
+++ src/main/kotlin/com/djumabaevs/data/requests/FollowUpdateRequest.kt
@@ -1,6 +1,5 @@
 package com.djumabaevs.data.requests
 
 data class FollowUpdateRequest(
-    val followingUserId: String,
     val followedUserId: String,
 )
