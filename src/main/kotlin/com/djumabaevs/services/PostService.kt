bfc31ffc6a6da18923935ed649fe42a1f559753c
Djumabaevs <djumabaevb@gmail.com>
2021-09-10 00:18:24 +0600

    post service refactor

--- src/main/kotlin/com/djumabaevs/services/PostService.kt
+++ src/main/kotlin/com/djumabaevs/services/PostService.kt
@@ -5,6 +5,7 @@ import com.djumabaevs.data.repository.post.PostRepository
 import com.djumabaevs.data.requests.CreatePostRequest
 import com.djumabaevs.util.Constants
 
+
 class PostService(
     private val repository: PostRepository
 ) {
@@ -26,6 +27,14 @@ class PostService(
         pageSize: Int = Constants.DEFAULT_POST_PAGE_SIZE
     ): List<Post> {
         return repository.getPostsByFollows(userId, page, pageSize)
+    }
+
+    suspend fun getPostsForProfile(
+        userId: String,
+        page: Int = 0,
+        pageSize: Int = Constants.DEFAULT_POST_PAGE_SIZE
+    ): List<Post> {
+        return repository.getPostsForProfile(userId, page, pageSize)
     }
 
     suspend fun getPost(postId: String): Post? = repository.getPost(postId)
